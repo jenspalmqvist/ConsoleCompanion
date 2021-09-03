5532b1123c3d6bc7b058aa08acde0c3a75bcfbf8
Jens Palmqvist <jpalmqvi@gmail.com>
2021-09-03 14:38:28 +0200

    Add project files.

---
+++ ConsoleCompanion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleCompanionTemplateNetCore31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cc = new ConsoleCompanion();
+        }
+    }
+}
